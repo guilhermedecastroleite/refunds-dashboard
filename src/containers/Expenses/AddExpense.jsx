b46099f0f3e00d979fdba24177db96f6d72ba374
Guilherme Leite <guilhermedecastroleite@gmail.com>
2021-01-15 22:21:52 -0300

    Adds formik and expenses form

--- src/containers/Expenses/AddExpense.jsx
+++ src/containers/Expenses/AddExpense.jsx
@@ -4,55 +4,79 @@ import {
 } from '@chakra-ui/react';
 import { FaReceipt } from 'react-icons/fa';
 import { CgNotes } from 'react-icons/cg';
+import { useFormik } from 'formik';
 
 import theme from '../../theme/theme';
 import AddExpenseForm from '../Forms/AddExpenseForm';
 
-const AddExpense = ({ boxProps }) => (
-  <Box w='100%' {...boxProps}>
-    {/** Top Buttons */}
-    <Flex justifyContent='flex-end'>
-      <Button
-        variant='outline'
-        leftIcon={<Icon as={CgNotes} />}
-      >
-        Inserir notas em lote
-      </Button>
-      <Button
-        ml='24px'
-        variant='outline'
-        leftIcon={<Icon as={FaReceipt} />}
-      >
-        Nova despesa
-      </Button>
-    </Flex>
+const AddExpense = ({ boxProps }) => {
+  const initialValues = {
+    file: null,
+    type: '',
+    title: '',
+    date: '',
+    receiptValue: '',
+    consideredReceiptValue: '',
+  };
 
-    {/** Form Card */}
-    <Box mt='24px' w='100%' boxShadow='md' borderRadius='6px' overflow='hidden'>
-      {/** Form */}
-      <Box pt='24px' pb='12px' px='48px' bg={theme.colors.offWhite}>
-        <Text {...theme.typography.lgBold} color={theme.colors.gray9}>Nova Despesa</Text>
-        <AddExpenseForm />
-      </Box>
+  const onSubmit = (values) => {
+    console.log('Submit: ', values);
+  };
 
-      {/** Card Bottom Bar */}
-      <Flex
-        h='77px'
-        pr='24px'
-        justifyContent='flex-end'
-        alignItems='center'
-        bg={theme.colors.white}
-      >
-        <Button variant='outline'>
-          Cancelar
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validateOnMount: true,
+  });
+
+  return (
+    <Box w='100%' {...boxProps}>
+      {/** Top Buttons */}
+      <Flex justifyContent='flex-end'>
+        <Button
+          variant='outline'
+          leftIcon={<Icon as={CgNotes} />}
+        >
+          Inserir notas em lote
         </Button>
-        <Button ml='24px' variant='solid'>
-          Salvar
+        <Button
+          ml='24px'
+          variant='outline'
+          leftIcon={<Icon as={FaReceipt} />}
+        >
+          Nova despesa
         </Button>
       </Flex>
+
+      {/** Form Card */}
+      <Box mt='24px' w='100%' boxShadow='md' borderRadius='6px' overflow='hidden'>
+        <form onSubmit={formik.handleSubmit}>
+          {/** Form */}
+          <Box pt='24px' pb='12px' px='48px' bg={theme.colors.offWhite}>
+            <Text {...theme.typography.lgBold} color={theme.colors.gray9}>Nova Despesa</Text>
+            <AddExpenseForm formik={formik} />
+          </Box>
+
+          {/** Card Bottom Bar */}
+          <Flex
+            h='77px'
+            pr='24px'
+            justifyContent='flex-end'
+            alignItems='center'
+            bg={theme.colors.white}
+          >
+            <Button variant='outline' type='button'>
+              Cancelar
+            </Button>
+            <Button ml='24px' variant='solid' type='submit'>
+              Salvar
+            </Button>
+          </Flex>
+        </form>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 AddExpense.propTypes = {
   boxProps: PropTypes.object,
--- src/containers/Forms/AddExpenseForm.jsx
+++ src/containers/Forms/AddExpenseForm.jsx
@@ -1,30 +1,112 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
 import {
-  Box, Button, Flex, Icon, Text,
+  Box, Button, Flex, Icon, Input, Select, Text,
 } from '@chakra-ui/react';
 import { FaQuestionCircle } from 'react-icons/fa';
 
 import theme from '../../theme/theme';
 
-const AddExpenseForm = () => (
-  <Flex>
-    <Box p='52px 92px' bg={theme.colors.white}>
-      <Flex alignItems='center'>
-        <Text {...theme.typography.mdBold} color={theme.colors.gray8}>
-          Recibo, cupom ou nota fiscal*
+const AddExpenseForm = ({ formik }) => {
+  const fileInput = useRef();
+
+  const onClickUpload = () => {
+    fileInput.current.click();
+  };
+
+  return (
+    <Flex>
+      {/** File Upload */}
+      <Box p='52px 92px' bg={theme.colors.white}>
+        <Flex alignItems='center'>
+          <Text {...theme.typography.mdBold} color={theme.colors.gray8}>
+            Recibo, cupom ou nota fiscal*
+          </Text>
+          <Icon ml='4px' as={FaQuestionCircle} color={theme.colors.gray2} />
+        </Flex>
+        <input
+          id='file'
+          type='file'
+          ref={fileInput}
+          onChange={(e) => formik.setFieldValue('file', e.target.files)}
+          style={{ display: 'none' }}
+        />
+        <Box mt='9px' p='110px 24px 44px 24px' border={`1px dashed ${theme.colors.gray12}`} borderRadius='3px'>
+          <Button
+            type='button'
+            variant='outline'
+            color={theme.colors.gray10}
+            borderColor={theme.colors.gray12}
+            onClick={onClickUpload}
+          >
+            Selecione um arquivo do seu computador
+          </Button>
+        </Box>
+        <Text {...theme.typography.xs} color={theme.colors.gray13}>
+          A imagem deve estar no formato JPG ou PNG.
         </Text>
-        <Icon ml='4px' as={FaQuestionCircle} color={theme.colors.gray2} />
-      </Flex>
-      <Box mt='9px' p='110px 24px 44px 24px' border={`1px dashed ${theme.colors.gray12}`} borderRadius='3px'>
-        <Button variant='outline' color={theme.colors.gray10} borderColor={theme.colors.gray12}>
-          Selecione um arquivo do seu computador
-        </Button>
       </Box>
-      <Text {...theme.typography.xs} color={theme.colors.gray13}>
-        A imagem deve estar no formato JPG ou PNG.
-      </Text>
-    </Box>
-    <Box ml='48px'>Inputs</Box>
-  </Flex>
-);
+
+      {/** Inputs */}
+      <Box ml='48px'>
+        <Text mb='12px' {...theme.typography.mdBold}>Tipo *</Text>
+        <Select
+          name='type'
+          type='text'
+          value={formik.values.type}
+          onChange={formik.handleChange('type')}
+          onBlur={formik.handleBlur('type')}
+          placeholder='Selecione'
+        />
+        <Text my='12px' {...theme.typography.mdBold}>Título da despesa *</Text>
+        <Input
+          name='title'
+          type='text'
+          value={formik.values.title}
+          onChange={formik.handleChange('title')}
+          onBlur={formik.handleBlur('title')}
+          placeholder='Ex: Almoço com colegas'
+        />
+        <Text my='12px' {...theme.typography.mdBold}>Data do comprovante *</Text>
+        <Input
+          name='date'
+          type='date'
+          value={formik.values.date}
+          onChange={formik.handleChange('date')}
+          onBlur={formik.handleBlur('date')}
+          placeholder='Selecione a data'
+        />
+        <Flex my='12px'>
+          <div>
+            <Text mb='12px' {...theme.typography.mdBold}>Valor da nota / cupom *</Text>
+            <Input
+              name='receiptValue'
+              type='number'
+              value={formik.values.receiptValue}
+              onChange={formik.handleChange('receiptValue')}
+              onBlur={formik.handleBlur('receiptValue')}
+              placeholder='Ex: 120'
+            />
+          </div>
+          <Box ml='45px'>
+            <Text mb='12px' {...theme.typography.mdBold}>Valor a ser considerado *</Text>
+            <Input
+              name='consideredReceiptValue'
+              type='number'
+              value={formik.values.consideredReceiptValue}
+              onChange={formik.handleChange('consideredReceiptValue')}
+              onBlur={formik.handleBlur('consideredReceiptValue')}
+              placeholder='Ex: 120'
+            />
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+};
+
+AddExpenseForm.propTypes = {
+  formik: PropTypes.object.isRequired,
+};
 
 export default AddExpenseForm;
