4b36897beb997800847f87631847194f2454ad2e
Guilherme Leite <guilhermedecastroleite@gmail.com>
2021-01-18 20:19:30 -0300

    Makes ActionBar responsive

--- src/containers/ActionBar/index.jsx
+++ src/containers/ActionBar/index.jsx
@@ -6,29 +6,44 @@ const ActionBar = () => (
   <Flex
     mt={['24px', '24px', '58px']}
     mb={['24px', '24px', '24px', '24px', 0]}
-    ml='-28px'
-    mr='-54px'
+    ml={['-16px', '-16px', '-28px']}
+    mr={['-16px', '-16px', '-54px']}
     p='24px 48px'
     alignItems='center'
     justifyContent='space-between'
+    flexDirection={['column', 'column', 'row']}
     bg={theme.colors.white}
   >
     <Button
       variant='outline'
       borderColor={theme.colors.red2}
       color={theme.colors.red2}
+      w={['100%', '100%', 'fit-content']}
     >
       Cancelar Solicitação
     </Button>
-    <Flex alignItems='center' justifyContent='center'>
+    <Flex
+      mt={['16px', '16px', 0]}
+      alignItems='center'
+      justifyContent='center'
+      flexDirection={['column', 'column', 'row']}
+      w={['100%', '100%', 'fit-content']}
+    >
       <Button
         variant='outline'
         borderColor={theme.colors.green4}
         color={theme.colors.green9}
+        w={['100%', '100%', 'fit-content']}
       >
         Continuar depois
       </Button>
-      <Button ml='24px'>Enviar para aprovação</Button>
+      <Button
+        mt={['16px', '16px', 0]}
+        ml={[0, 0, '24px']}
+        w={['100%', '100%', 'fit-content']}
+      >
+        Enviar para aprovação
+      </Button>
     </Flex>
   </Flex>
 );
