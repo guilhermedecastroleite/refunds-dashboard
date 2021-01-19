99ee0addd48ef393a0d79cb40bd3a1b25d7eb5b6
Guilherme Leite <guilhermedecastroleite@gmail.com>
2021-01-19 11:30:50 -0300

    Makes side menu collapsable

--- src/components/Layout/BasePage.jsx
+++ src/components/Layout/BasePage.jsx
@@ -1,27 +1,75 @@
+import { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Icon } from '@chakra-ui/react';
+import { FaBars } from 'react-icons/fa';
 
 import theme from '../../theme/theme';
+import { useWindowSize } from '../../hooks/useWindowSize';
 
-const BasePage = ({ children }) => (
-  <Box overflow='auto' minHeight='100vh'>
-    <Box w='100%' h={['64px', '64px', '64px', '64px', '64px', '48px']} zIndex={9999} bg={theme.colors.blue4} position='fixed' top={0} />
-    <Flex>
-      <Box
-        w='220px'
-        h='100vh'
+const BasePage = ({ children }) => {
+  const size = useWindowSize();
+  const menuRef = useRef();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleClick = (e) => {
+    if (menuRef.current.contains(e.target)) {
+      return;
+    }
+    setShowMenu(false);
+  };
+
+  useEffect(() => {
+    setShowMenu(size.width > 1536);
+  }, [size]);
+
+  useEffect(() => {
+    // Mount
+    document.addEventListener('mousedown', handleClick);
+    // Unmount
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, []);
+
+  return (
+    <Box overflow='auto' minHeight='100vh'>
+      <Flex
+        w='100%'
+        px='24px'
+        h={['64px', '64px', '64px', '64px', '64px', '48px']}
         zIndex={9999}
         bg={theme.colors.blue4}
         position='fixed'
-        left={0}
-        display={['none', 'none', 'none', 'none', 'none', 'initial']}
-      />
-      <Box w='100%' mt={['64px', '64px', '64px', '64px', '64px', '48px']} ml={[0, 0, 0, 0, 0, '220px']}>
-        {children}
-      </Box>
-    </Flex>
-  </Box>
-);
+        top={0}
+        flexDirection='column'
+        justifyContent='center'
+      >
+        <Icon
+          as={FaBars}
+          color={theme.colors.white}
+          fontSize='24px'
+          display={['initial', 'initial', 'initial', 'initial', 'initial', 'none']}
+          onClick={() => setShowMenu(true)}
+        />
+      </Flex>
+      <Flex>
+        <Box
+          w='220px'
+          h='100vh'
+          zIndex={9999}
+          bg={theme.colors.blue4}
+          position='fixed'
+          left={showMenu ? 0 : '-220px'}
+          transition='all 300ms ease-in-out'
+          ref={menuRef}
+        />
+        <Box w='100%' mt={['64px', '64px', '64px', '64px', '64px', '48px']} ml={[0, 0, 0, 0, 0, '220px']}>
+          {children}
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
 
 BasePage.propTypes = {
   children: PropTypes.any,
